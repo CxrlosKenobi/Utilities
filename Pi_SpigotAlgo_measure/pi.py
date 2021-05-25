8d8662f37044c206a5b11960baa4ad47023320ff
Carlos <yolitopinto@gmail.com>
2021-05-25 11:57:47 -0400

    Update pi.py syntax

--- Pi_SpigotAlgo_measure/pi.py
+++ Pi_SpigotAlgo_measure/pi.py
@@ -3,32 +3,32 @@ import time
 def spigot(measure, i):
     try:
         x = []
-        q,r,t,k,n,l = 1,0,1,1,3,3
+        q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
         start = time.time()
 
         if measure == False:
             while True:
-                if 4*q+r-t < n*t:
+                if 4*q + r-t < n * t:
                     x.append(n)
-                    q,r,t,k,n,l = (
-                        10*q,10*(r-n*t),t,k,
-                        (10*(3*q+r))//t-10*n,l)
+                    q, r, t, k, n, l = (
+                        10*q, 10*(r - n*t), t, k,
+                        (10 * (3*q + r)) // t- 10*n, l)
                 else:
-                    q,r,t,k,n,l = (
-                        q*k,(2*q+r)*l,t*l,k+1,
-                        (q*(7*k+2)+r*l)//(t*l),l+2)
+                    q, r, t, k, n, l = (
+                        q * k, (2*q + r) * l, t * l, k + 1,
+                        (q * (7*k +2)+ r*l) // (t * l), l + 2)
                 print(f'[{len(x)}]')
         elif measure == True:
             while len(x) < i:
-                if 4*q+r-t < n*t:
+                if 4*q + r - t < n*t:
                     x.append(n)
-                    q,r,t,k,n,l = (
-                        10*q,10*(r-n*t),t,k,
-                        (10*(3*q+r))//t-10*n,l)
+                    q, r, t, k, n, l = (
+                        10 * q, 10 * (r-n * t), t, k,
+                        (10 * (3*q + r)) // t - 10*n, l)
                 else:
-                    q,r,t,k,n,l = (
-                        q*k,(2*q+r)*l,t*l,k+1,
-                        (q*(7*k+2)+r*l)//(t*l),l+2)
+                    q, r, t, k, n, l = (
+                        q * k, (2*q + r) * l, t * l, k + 1,
+                        (q * (7*k + 2)+ r*l) // (t * l), l + 2)
                 print(f'[{len(x)}]')
             end = time.time()
             print('\n\nElapsed Time: ' + f'{round(end-start, 4)}s')
