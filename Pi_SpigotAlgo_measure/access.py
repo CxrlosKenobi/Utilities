426d983c0dc863a938856d70be33d505c39eb929
CxrlosKenobi <kenobi@ieee.org>
2022-07-19 00:26:14 -0400

    Massive fetch

---
+++ Pi_SpigotAlgo_measure/access.py
@@ -0,0 +1,22 @@
+# Given an local IP address, it will help to connect the device through SSH.
+
+targetData = {
+    "ip": "192.168.1.195",
+    "os": "w10",
+    "user": "benja",
+    "password": "benja1212"
+}
+
+#- Script that enables the ssh connection of the windows device.
+    # The script will be executed on the device.
+
+
+#- Check if the target is vulnerable to the attack.
+#- If the target is vulnerable, then connect to the target and execute the attack.
+    #- Connect to the target device through SSH.
+    #- Scan ports
+
+#- (Auxiliar func) Detect local IP address
+    # f'sudo nmap -sP {hostname}/24'
+
+
